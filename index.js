2e2a01a119790cba7afd48bc092c0e312131f0c1
jeel narola <narolajeel29@gmail.com>
2024-08-04 12:30:11 +0530

    Signup | Login API Test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express')
+const cors=require("cors")
 const database = require('./configs/db')
 const router = require('./routers/router')
 
@@ -7,7 +8,11 @@ const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(router)
+app.use(cors())
+// app.use(router)
+app.get("/",(req,res)=>{
+    res.json({msg:"hello"})
+})
 
 app.listen(8090,()=>{
     console.log("Server Start...")
--- routers/user/userRouter.js
+++ routers/user/userRouter.js
@@ -3,10 +3,10 @@ const { signup, login } = require('../../controllers/user/userController')
 
 const userRouter=Router()
 
-userRouter.post("/signup",signup)
-userRouter.post("/login",login)
-userRouter.get("/s",(req,res)=>{
-    res.json({msg:"hello"})
+// userRouter.post("/signup",signup)
+// userRouter.post("/login",login)
+userRouter.get("/home",(req,res)=>{
+    res.json({msg:"hello Home"})
 })
 
 
