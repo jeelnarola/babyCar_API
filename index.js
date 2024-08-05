2db863a428e90910d37687f0aa6190e98feaeb23
jeel narola <narolajeel29@gmail.com>
2024-08-05 10:46:57 +0530

    Testing..

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const database = require('./configs/db')
 const router = require('./routers/router')
 const proRouter = require('./routers/product/productRouter')
 require('dotenv').config()
+
+const corsConfig={
+    origin:"*",
+    credential:true,
+    method:["GET","POST","PATCH","PUT","DELETE"]
+}
+
+
 const app=express()
 
 
 app.use(express.json())
+
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.options("",cors(corsConfig))
+app.use(cors(corsConfig))
+
 app.use(proRouter)
 app.use(router)
 
